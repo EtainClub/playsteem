441081e08732b6690a1e049817cf6c2f52c2420f
etain <etainclub@gmail.com>
2021-04-02 00:51:55 +0900

    code push release 0.9.7

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,6 +1,6 @@
-export const APP_ANDROID_VERSION = '0.9.6';
+export const APP_ANDROID_VERSION = '0.9.7';
 export const GOOGLEPLAY = '';
-export const APP_IOS_VERSION = '0.9.6';
+export const APP_IOS_VERSION = '0.9.7';
 export const APPSTORE = '';
 export const SOURCE_URL = 'https://github.com/EtainClub/playsteem';
 export const TERMS_URL = 'https://playsteem.app/terms';
